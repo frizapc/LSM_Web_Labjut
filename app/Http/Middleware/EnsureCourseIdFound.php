06b33939e528a0e80dccb22795b11ea9d0e6c41b
frizalaptop <frizamuhammadcrb1234@gmail.com>
2025-06-25 12:34:38 +0700

    add materials entity

---
+++ app/Http/Middleware/EnsureCourseIdFound.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\Course;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureCourseIdFound
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        Course::findOrFail($request->course);
+        return $next($request);
+    }
+}
---
+++ database/migrations/2025_06_25_030210_create_materials_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('materials', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->text('description')->nullable();
+            $table->string('source');
+            $table
+                ->foreignId('course_id')->constrained(
+                    table: 'courses', indexName: 'materials_course_id'
+                )
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('materials');
+    }
+};
